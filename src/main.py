782a889d114192f6362b3508bca492976b1d84ed
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-12-20 13:48:40 -0800

    use fastapi instead of flask

--- src/main.py
+++ src/main.py
@@ -1,15 +1,10 @@
 import os
 
-from flask import Flask
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
-
-@app.route("/")
+@app.get("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
     return "Hello {}!".format(name)
-
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
