b66ff31d7081100168f2b48e98ce39f4937c6ff2
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-12-20 14:01:03 -0800

    make / a POST route

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
-import os
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
 
 app = FastAPI()
 
-@app.get("/")
-def hello_world():
-    name = os.environ.get("NAME", "World")
+@app.post("/", status_code=status.HTTP_202_ACCEPTED)
+async def hello_world(request: Request):
+    payload = await request.json()
+    name = payload["name"]
     return "Hello {}!".format(name)
