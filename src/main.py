95ec62c181a03bc3a57e907094d25414525a5ff5
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-12-21 20:29:53 -0800

    renaming and reorganizing

--- src/main.py
+++ src/main.py
@@ -8,10 +8,6 @@ from pydantic import BaseModel, Field
 
 app = FastAPI()
 
-# we assume we're always working on top of a pangeo/forge image tag,
-# in which the default env linked to PYTHONPATH is named "notebook"
-CONDA_ENV = "notebook"
-
 
 class PangeoForgeRunner(BaseModel):
     cmd: List[str] = Field(
@@ -19,7 +15,7 @@ class PangeoForgeRunner(BaseModel):
         description="""
         Command to pass to the `pangeo-forge-runner` CLI. The command should be valid
         for the version of `pangeo-forge-runner` known to exist in the container.
-        (This can be overided via the `pip_install` field of this Payload.)
+        (This can be overided via the `install` field of the request Payload.)
         """
     )
     config: dict = Field(
@@ -33,6 +29,23 @@ class PangeoForgeRunner(BaseModel):
     )
 
 
+class Install(BaseModel):
+    pkgs: List[str] = Field(
+        ...,
+        description="""
+        A list of extra dependencies to install prior to calling `pangeo-forge-runner`.
+        (Can include `pangeo-forge-runner` itself!) List members can be strings of any
+        format recognized as valid arguments to `pip install`.
+        """
+    )
+    env: str = Field(
+        "notebook",
+        description="""
+        The name of the conda environment in which to pip install the `pkgs` list.
+        """
+    )
+
+
 class Payload(BaseModel):
     pangeo_forge_runner: PangeoForgeRunner = Field(
         ...,
@@ -40,17 +53,16 @@ class Payload(BaseModel):
         A command and config for `pangeo-forge-runner`.
         """
     ) 
-    pip_install: Optional[List[str]] = Field(
+    install: Optional[Install] = Field(
         None,
         description="""
-        Optionally, pass a list of extra dependencies to install prior to calling
-        `pangeo-forge-runner`. List members can be strings of any format
-        recognized as valid arguments to `pip install`.
+        Optionally, use this field to pass a list of extra dependencies to install
+        prior to calling `pangeo-forge-runner`.
         """
     )
 
 
-class PipResult(BaseModel):
+class InstallResult(BaseModel):
     before: List[dict] = Field(
         ...,
         description="""
@@ -76,19 +88,19 @@ class PipResult(BaseModel):
 
 
 class Response(BaseModel):
-    pip_result: Optional[PipResult] = Field(
+    install_result: Optional[InstallResult] = Field(
         None,
         description="""
-        If a `pip_install` array is passed as part of the request Payload, the results
+        If an `install` array is passed as part of the request Payload, the results
         of resolving those dependencies is relayed back to the invoker here. Optional
-        because if no `pip install` array is given in the request, this is left empty. 
+        because if no `install` array is given in the request, this is left empty. 
         """
     )
     pangeo_forge_runner_result: Optional[str] = Field(
         None,
         description="""
         The result of the `pangeo-forge-runner` call as plain text, which the invoker
-        can parse as they please. Optional because if a `pip_install` array is given
+        can parse as they please. Optional because if an `install` array is given
         in the request, and there is an error attempting to resolve those additional
         dependencies, then we never make it to calling `pangeo-forge-runner`, and this
         field would be left empty in the response.
@@ -96,28 +108,30 @@ class Response(BaseModel):
     )
 
 
-def conda_list_json():
+def conda_list_json(env_name):
     return json.loads(
-        subprocess.check_output(f"conda list -n {CONDA_ENV} --json".split())
+        subprocess.check_output(f"conda list -n {env_name} --json".split())
     )
 
 
 @app.post("/", status_code=status.HTTP_202_ACCEPTED, response_model=Response)
 async def main(payload: Payload):
     response = {}
-    if payload.pip_install:
-        response |= {"pip_result": {"before": conda_list_json()}}
-        pip_proc = subprocess.run(
-            f"mamba run -n {CONDA_ENV} pip install -U".split() + payload.pip_install,
-            capture_output=True,
-            text=True,
+    if payload.install:
+        response |= {
+            "install_result": {"before": conda_list_json(payload.install.env)}
+        }
+        cmd = (
+            f"mamba run -n {payload.install.env} pip install -U".split()
+            + payload.install.pkgs
         )
-        if pip_proc.returncode != 0:
-            # our pip installations failed, so record the error and bail early
-            response["pip_result"] |= {"stderr": pip_proc.stderr}
+        proc = subprocess.run(cmd, capture_output=True, text=True)
+        if proc.returncode != 0:
+            # our installations failed, so record the error and bail early
+            response["install_result"] |= {"stderr": proc.stderr}
             return response
-        # our pip installations succeeded! so record the altered env and move on
-        response["pip_result"] |= {"after": conda_list_json()}
+        # our installations succeeded! so record the altered env and move on
+        response["install_result"] |= {"after": conda_list_json(payload.install.env)}
 
     with tempfile.NamedTemporaryFile("w", suffix=".json") as f:
         json.dump(payload.pangeo_forge_runner.config, f)
