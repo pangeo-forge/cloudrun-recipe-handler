32baab193ab520e20188193acc84bdbcc4389df5
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-12-23 12:03:32 -0800

    use env.yaml, and parametrize test

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi.testclient import TestClient
 
 from ..main import app
@@ -5,15 +6,42 @@ from ..main import app
 client = TestClient(app)
 
 
-def test_main():
+@pytest.mark.parametrize(
+    "pkgs,expected_diff",
+    [
+        (
+            ["pangeo-forge-runner==0.7.1"],
+            # ./ci/env.yaml already has "pangeo-forge-runner==0.7.1",
+            # therefore passing it here should result in no diff
+            {"added": [], "changed": []},
+        ),
+        (
+            ["pangeo-forge-runner==0.7.0"],
+            # if we request a different version of pangeo-forge-runner from
+            # the one which comes pre-installed in our env, then of course
+            # we do expect to see a diff reported by the cloudrun service
+            {
+                "added": [],
+                "changed": [
+                    {
+                        "name": "pangeo-forge-runner",
+                        "version": "0.7.0",
+                        "prior_version": "0.7.1",
+                    }
+                ],
+            },
+        ),
+    ],
+)
+def test_main(pkgs, expected_diff):
     request = {
         "pangeo_forge_runner": {"cmd": ["--help"]},
-        "install": {"pkgs": ["pangeo-forge-runner==0.7.1"], "env": "cloudrun"},
+        "install": {"pkgs": pkgs, "env": "cloudrun"},
     }
     response = client.post("/", json=request)
     assert response.status_code == 202
     assert response.json()["install_result"] == {
-        "diff": {"added": [], "changed": []},
+        "diff": expected_diff,
         "stderr": None,
     }
     assert response.json()["pangeo_forge_runner_result"].startswith(
