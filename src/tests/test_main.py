5ef75c364cdd61e7cef285485895ccf9d11d8c40
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-12-23 13:19:00 -0800

    test stderr, reset version at end of test session

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,3 +1,5 @@
+import subprocess
+
 import pytest
 from fastapi.testclient import TestClient
 
@@ -5,15 +7,32 @@ from ..main import app
 
 client = TestClient(app)
 
+# note, if bumping this, you must do so in ./ci/env.yaml as well
+STARTING_VERSION = "pangeo-forge-runner==0.7.1"
+
+
+@pytest.fixture(autouse=True)
+def setup_teardown():
+    yield
+    # the tests change the underlying conda environment, so let's reset that
+    # before exiting the test session, so that the developer doesn't have to
+    # manually do so before running the next test session. this doesn't matter
+    # for ci, but for local development it's very helpful
+    proc = subprocess.run(
+        f"mamba run -n cloudrun pip install -U {STARTING_VERSION}".split()
+    )
+    assert proc.returncode == 0
+
 
 @pytest.mark.parametrize(
-    "pkgs,expected_diff",
+    "pkgs,expected_diff,expected_error",
     [
         (
-            ["pangeo-forge-runner==0.7.1"],
-            # ./ci/env.yaml already has "pangeo-forge-runner==0.7.1",
+            [STARTING_VERSION],
+            # ./ci/env.yaml already has this version,
             # therefore passing it here should result in no diff
             {"added": [], "changed": []},
+            None,
         ),
         (
             ["pangeo-forge-runner==0.7.0"],
@@ -26,24 +45,34 @@ client = TestClient(app)
                     {
                         "name": "pangeo-forge-runner",
                         "version": "0.7.0",
-                        "prior_version": "0.7.1",
+                        "prior_version": STARTING_VERSION.split("==")[-1],
                     }
                 ],
             },
+            None,
+        ),
+        (
+            ["pangeo-forge-runner==0.7.05"],
+            # this version doesn't exist, so pip will throw an error
+            None,
+            "ERROR: No matching distribution found for pangeo-forge-runner==0.7.05",
         ),
     ],
 )
-def test_main(pkgs, expected_diff):
+def test_main(pkgs, expected_diff, expected_error):
     request = {
         "pangeo_forge_runner": {"cmd": ["--help"]},
         "install": {"pkgs": pkgs, "env": "cloudrun"},
     }
     response = client.post("/", json=request)
     assert response.status_code == 202
-    assert response.json()["install_result"] == {
-        "diff": expected_diff,
-        "stderr": None,
-    }
-    assert response.json()["pangeo_forge_runner_result"].startswith(
-        "This is an application.\n\n"
-    )
+    assert response.json()["install_result"]["diff"] == expected_diff
+
+    if expected_error:
+        assert expected_error in response.json()["install_result"]["stderr"]
+        assert response.json()["pangeo_forge_runner_result"] is None
+    else:
+        assert response.json()["install_result"]["stderr"] is None
+        assert response.json()["pangeo_forge_runner_result"].startswith(
+            "This is an application.\n\n"
+        )
