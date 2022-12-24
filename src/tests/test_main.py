5ff3efe73d1bf5d7a504fa235f1bcd3bffae614b
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-12-23 16:10:54 -0800

    reorganize tests for clarity

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -2,82 +2,113 @@ import subprocess
 
 import pytest
 from fastapi.testclient import TestClient
+from pytest_lazyfixture import lazy_fixture
 
 from ..main import app
 
 client = TestClient(app)
 
-# note, if bumping this, you must do so in ./ci/env.yaml as well
-# NOTE: because tests alter environment in a pre-determined sequence,
-# they cannot be run individually or out-of-sequence.
-STARTING_VERSION = "pangeo-forge-runner==0.7.1"
+DEFAULT_RUNNER_VERSION = "pangeo-forge-runner==0.7.1"
 
 
-@pytest.fixture(autouse=True)
-def setup_teardown():
-    yield
-    # the tests change the underlying conda environment, so let's reset that
-    # before exiting the test session, so that the developer doesn't have to
-    # manually do so before running the next test session. this doesn't matter
-    # for ci, but for local development it's very helpful
-    proc = subprocess.run(
-        f"mamba run -n cloudrun pip install -U {STARTING_VERSION}".split()
-    )
+def call_pip(cmd, pkgs):
+    proc = subprocess.run(f"mamba run -n cloudrun pip {cmd} {' '.join(pkgs)}".split())
     assert proc.returncode == 0
 
 
-@pytest.mark.parametrize(
-    "cmd,pkgs,expected_diff,expected_error",
-    [
-        (
-            ["--help"],
-            [STARTING_VERSION],
-            # ./ci/env.yaml already has this version,
-            # therefore passing it here should result in no diff
-            {"added": [], "changed": []},
-            None,
-        ),
-        (
-            ["--help"],
-            # test adding a pkg not in the base env
-            ["black==22.12.0"],
-            {"added": [{"name": "black", "version": "22.12.0"}], "changed": []},
-            None,
-        ),
-        (
-            ["bake", "--unsupported-arg"],
-            [STARTING_VERSION],
-            {"added": [], "changed": []},
-            "error: unrecognized arguments: --unsupported-arg\n",
-        ),
-        (
-            ["--help"],
-            ["pangeo-forge-runner==0.7.0"],
-            # if we request a different version of pangeo-forge-runner from
-            # the one which comes pre-installed in our env, then of course
-            # we do expect to see a diff reported by the cloudrun service
+@pytest.fixture
+def no_install_no_error():
+    cmd = ["--help"]
+    pkgs = [DEFAULT_RUNNER_VERSION]
+    expected_diff = {"added": [], "changed": []}
+    expected_error = None
+
+    call_pip("install -U", pkgs)
+
+    yield cmd, pkgs, expected_diff, expected_error
+
+
+@pytest.fixture
+def add_pkg():
+    added_pkg = "black==22.12.0"
+
+    cmd = ["--help"]
+    pkgs = [DEFAULT_RUNNER_VERSION, added_pkg]
+    expected_diff = {"added": [{"name": "black", "version": "22.12.0"}], "changed": []}
+    expected_error = None
+
+    # make sure that black will be the only pkg added under test
+    call_pip("uninstall -y", [added_pkg])
+    call_pip("install -U", [DEFAULT_RUNNER_VERSION])
+
+    yield cmd, pkgs, expected_diff, expected_error
+
+
+@pytest.fixture
+def runner_called_proc_error():
+    cmd = ["bake", "--unsupported-arg"]
+    pkgs = [DEFAULT_RUNNER_VERSION]
+    expected_diff = {"added": [], "changed": []}
+    expected_error = "error: unrecognized arguments: --unsupported-arg\n"
+
+    call_pip("install -U", pkgs)
+
+    yield cmd, pkgs, expected_diff, expected_error
+
+
+@pytest.fixture
+def change_runner_version():
+    cmd = ["--help"]
+    pkgs = ["pangeo-forge-runner==0.7.0"]
+    # if we request a different version of pangeo-forge-runner from
+    # the one which comes pre-installed in our env, then we expect
+    # to see a diff reported by the service
+    expected_diff = {
+        "added": [],
+        "changed": [
             {
-                "added": [],
-                "changed": [
-                    {
-                        "name": "pangeo-forge-runner",
-                        "version": "0.7.0",
-                        "prior_version": STARTING_VERSION.split("==")[-1],
-                    }
-                ],
-            },
-            None,
-        ),
-        (
-            ["--help"],
-            ["pangeo-forge-runner==0.7.05"],
-            # this version doesn't exist, so pip will throw an error
-            None,
-            "ERROR: No matching distribution found for pangeo-forge-runner==0.7.05",
-        ),
-    ],
+                "name": "pangeo-forge-runner",
+                "version": "0.7.0",
+                "prior_version": DEFAULT_RUNNER_VERSION.split("==")[-1],
+            }
+        ],
+    }
+    expected_error = None
+
+    # make sure default version is installed here, so that we can be sure the
+    # changed version will be installed by the service under test
+    call_pip("install -U", [DEFAULT_RUNNER_VERSION])
+
+    yield cmd, pkgs, expected_diff, expected_error
+
+
+@pytest.fixture
+def installation_error():
+    cmd = ["--help"]
+    # this version doesn't exist on pypi, which is what we want for this test!
+    pkgs = ["pangeo-forge-runner==0.7.05"]
+    expected_diff = {"added": [], "changed": []}
+    expected_error = (
+        "ERROR: No matching distribution found for pangeo-forge-runner==0.7.05"
+    )
+    yield cmd, pkgs, expected_diff, expected_error
+
+
+@pytest.fixture(
+    params=[
+        lazy_fixture("no_install_no_error"),
+        lazy_fixture("add_pkg"),
+        lazy_fixture("runner_called_proc_error"),
+        lazy_fixture("change_runner_version"),
+        lazy_fixture("installation_error"),
+    ]
 )
-def test_main(cmd, pkgs, expected_diff, expected_error):
+def fixture(request):
+    return request.param
+
+
+def test_main(fixture):
+    cmd, pkgs, expected_diff, expected_error = fixture
     request = {
         "pangeo_forge_runner": {"cmd": cmd},
         "install": {"pkgs": pkgs, "env": "cloudrun"},
